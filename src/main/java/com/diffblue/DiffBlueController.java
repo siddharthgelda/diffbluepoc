cfe122310ca28debba18243d492df5c6d66f0c3c
Sid Geleda <geldasiddharth@gmail.com>
2024-11-07 04:51:29 -0800

    Diffblue POC

--- src/main/java/com/diffblue/DiffBlueController.java
+++ src/main/java/com/diffblue/DiffBlueController.java
@@ -3,6 +3,7 @@ package com.diffblue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,13 +11,13 @@ public class DiffBlueController {
 @Autowired
 DiffBlueServcie diffBlueServcie;
     @GetMapping
-    public ResponseEntity validateEmail(String emailId){
+    public ResponseEntity<String> validateEmail(@RequestParam("emailId")String emailId){
         boolean result = diffBlueServcie.validateEmailid(emailId);
         if(result)
         {
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok("valid EmailId");
         }else{
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body("EmailId not valid");
         }
 
     }
